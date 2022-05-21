aafce291919129adab5aacac6f53b503950626da
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-21 13:25:13 -0300

    estrutura inicial do projeto

---
+++ src/test/java/com/agrotis/agrotis/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis.agrotis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
