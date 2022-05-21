dfa3925099f357f6a28abce20cc268fe62a3e981
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-21 20:41:24 -0300

    feat: adiciona novas exceções

---
+++ src/main/java/com/agrotis/agrotis/Exceptions/ErroChaveDate.java
@@ -0,0 +1,7 @@
+package com.agrotis.agrotis.Exceptions;
+
+public class ErroChaveDate extends ErroDeChave {
+  public ErroChaveDate() {
+    super("Datas precisam ser preenchidas");
+  }
+}
---
+++ src/main/java/com/agrotis/agrotis/Exceptions/ErroChaveName.java
@@ -0,0 +1,7 @@
+package com.agrotis.agrotis.Exceptions;
+
+public class ErroChaveName  extends ErroDeChave{
+  public ErroChaveName() {
+    super("Name precisa ser uma String valida");
+  }
+}
