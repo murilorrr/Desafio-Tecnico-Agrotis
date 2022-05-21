73e0c4601eaff62db1b5cbda306c7f0eae42adb1
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-21 16:58:10 -0300

    feat: adiciona metodo para deletar usuário

--- src/main/java/com/agrotis/agrotis/controllers/UserController.java
+++ src/main/java/com/agrotis/agrotis/controllers/UserController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.agrotis.agrotis.Entities.User;
 import com.agrotis.agrotis.repositories.UserRepository;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +25,20 @@ public class UserController {
     return userRepository.findAll();
   }
 
+  @GetMapping("/users/{id}")
+  public User getById(@PathVariable Long id) {
+    return userRepository.findById(id).get();
+  }
+
   @PostMapping("/users")
   public User create(@RequestBody User user) {
     return userRepository.save(user);
+  }
+
+  @DeleteMapping("/users/{id}")
+  public void delete(@PathVariable Long id) {
+    userRepository.deleteById(id);
+    return;
   }
 
 }
