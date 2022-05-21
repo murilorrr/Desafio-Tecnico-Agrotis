6087a3bd4765928bfc7194d97a1c10b126715f4b
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-21 20:27:52 -0300

    feat: adiciona metodo abstrato de procurar pelo nome no repositorio de propriedades

--- src/main/java/com/agrotis/agrotis/repositories/PropriedadeRepository.java
+++ src/main/java/com/agrotis/agrotis/repositories/PropriedadeRepository.java
@@ -1,10 +1,13 @@
 package com.agrotis.agrotis.repositories;
 
 
+import java.util.List;
+
 import com.agrotis.agrotis.Entities.Propriedade;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropriedadeRepository extends JpaRepository<Propriedade, Long>{
   public Propriedade findOneByName(String name);
+  public List<Propriedade> findByName(String name);
 }
