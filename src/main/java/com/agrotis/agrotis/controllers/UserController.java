09590804394df0cc0ffde2616811fa88fec2fee8
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-21 16:15:33 -0300

    fix: adiciona construtor com todos os argumentos (repository)

--- src/main/java/com/agrotis/agrotis/controllers/UserController.java
+++ src/main/java/com/agrotis/agrotis/controllers/UserController.java
@@ -6,9 +6,14 @@ import com.agrotis.agrotis.Entities.User;
 import com.agrotis.agrotis.repositories.UserRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class UserController {
 
   UserRepository userRepository;
@@ -16,6 +21,11 @@ public class UserController {
   @GetMapping("/users")
   public List<User> getAll() {
     return userRepository.findAll();
+  }
+
+  @PostMapping("/users")
+  public User create(@RequestBody User user) {
+    return userRepository.save(user);
   }
 
 }
