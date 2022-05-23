9792d6a5210fc5c9bf38311821cf0d34f89645fc
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-23 08:09:30 -0300

    adiciona uma classe mensagem de erro padrão

---
+++ src/main/java/com/agrotis/agrotis/messages/ErrorMessageDefault.java
@@ -0,0 +1,41 @@
+package com.agrotis.agrotis.messages;
+
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ErrorMessageDefault {
+  private Instant instant;
+  private int status;
+  private String message;
+
+  public ErrorMessageDefault() {
+
+  }
+
+  public Instant getInstant() {
+    return instant;
+  }
+
+  public void setInstant(Instant instant) {
+    this.instant = instant;
+  }
+
+  public int getStatus() {
+    return status;
+  }
+
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+}
