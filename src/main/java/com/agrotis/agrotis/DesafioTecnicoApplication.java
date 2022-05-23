0b0053952a96fa1f349cc867fbcc141d122e9358
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-23 07:55:29 -0300

    style: utilizando checkstyle para formatação de codigo

--- src/main/java/com/agrotis/agrotis/DesafioTecnicoApplication.java
+++ src/main/java/com/agrotis/agrotis/DesafioTecnicoApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesafioTecnicoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioTecnicoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DesafioTecnicoApplication.class, args);
+  }
 
 }
