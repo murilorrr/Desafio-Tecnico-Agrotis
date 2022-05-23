b02e46c3249b4623324d9c1f5026b8ee731778fb
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-23 08:38:02 -0300

    style: utilizando checkstyle para formatação de codigo

--- src/test/java/com/agrotis/agrotis/UserControllerTest.java
+++ src/test/java/com/agrotis/agrotis/UserControllerTest.java
@@ -1,5 +1,9 @@
 package com.agrotis.agrotis;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.agrotis.agrotis.repositories.UserRepository;
 
 import org.junit.jupiter.api.Test;
@@ -9,8 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -27,14 +29,16 @@ public class UserControllerTest {
     mockMvc.perform(get("/users"))
     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
     .andExpect(status().isOk())
-    .andExpect(content().json("{users: []}"));
+      .andExpect(content().json("{users: []}"));
   }
 
   @Test
   void deve_ser_possivel_adicionar_usuarios() throws Exception {
 
-    // InputStream getLocalJsonFile = new FileInputStream("src/test/java/com/agrotis/agrotis/user.json");
-    // HashMap<String,String> jsonMap = new ObjectMapper().readValue(getLocalJsonFile, HashMap.class);    
+    // InputStream getLocalJsonFile = new FileInputStream(
+      // "src/test/java/com/agrotis/agrotis/user.json");
+    // HashMap<String,String> jsonMap = new ObjectMapper().
+    // readValue(getLocalJsonFile, HashMap.class);
 
     // mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON)
     // .content(jsonMap.toString()))
