5b93c63b0327f5671b51c0345d1755e8ded56e93
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-23 09:52:58 -0300

    fix: adiciona uma rota de hello world no app spring

--- src/main/java/com/agrotis/agrotis/controllers/HelloWorldController.java
+++ src/main/java/com/agrotis/agrotis/controllers/HelloWorldController.java
@@ -1,22 +1,15 @@
 package com.agrotis.agrotis.controllers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@SpringBootApplication
+@RestController
 public class HelloWorldController {
 
   @RequestMapping("/")
   @ResponseBody
   String home() {
     return "Hello World!";
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(HelloWorldController.class, args);
   }
 }
