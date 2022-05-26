36260970156fea1066e4ccba5274703b9b02c66f
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-26 11:12:03 -0300

    adiciona configuração teste para rodar spring

---
+++ src/test/java/com/agrotis/AgrotisApplicationTests.java
@@ -0,0 +1,17 @@
+package com.agrotis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@SpringBootConfiguration
+@ContextConfiguration
+class AgrotisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ src/test/java/com/agrotis/controllers/LaboratorioControllerTest.java
@@ -0,0 +1,31 @@
+// package com.agrotis.controllers;
+
+// import com.agrotis.agrotis.controllers.LaboratorioController;
+// import com.agrotis.agrotis.repositories.LaboratorioRepository;
+
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+
+// public class LaboratorioControllerTest {
+  
+//   private LaboratorioController controller;
+
+//   // @BeforeEach
+//   // public void setup() {
+//   //   RestAssured.standAlone();
+//   // }
+
+//   @Test
+//   public void deveConterUmaListaDeLaboratorios_QuandoChamarFindAll() {
+//     LaboratorioRepository repository;
+//     // when controller.findAll()
+//     // mock repository.findAll() devolve [];
+//     // return []
+
+
+//   }
+
+
+// }
