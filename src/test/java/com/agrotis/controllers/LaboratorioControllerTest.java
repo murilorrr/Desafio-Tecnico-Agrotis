d3854f8efbd99c67aacce5bb7968cc270d3d4d78
Murilo Ribeiro <murilorsv14@gmail.com>
2022-06-03 23:49:23 -0300

    test: adiciona alguns testes de unidade para o controller de laboratorio

--- src/test/java/com/agrotis/controllers/LaboratorioControllerTest.java
+++ src/test/java/com/agrotis/controllers/LaboratorioControllerTest.java
@@ -1,31 +1,68 @@
 package com.agrotis.controllers;
 
-// import com.agrotis.agrotis.controllers.LaboratorioController;
-// import com.agrotis.agrotis.repositories.LaboratorioRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
-// import org.springframework.beans.factory.annotation.Autowired;
+import com.agrotis.agrotis.controllers.LaboratorioController;
+import com.agrotis.agrotis.entities.Laboratorio;
+import com.agrotis.agrotis.repositories.LaboratorioRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Teste de unidade para o controller de laboratorios")
 public class LaboratorioControllerTest {
   
-//   private LaboratorioController controller;
+  @MockBean
+  private LaboratorioController controller;
 
-//   // @BeforeEach
-//   // public void setup() {
-//   //   RestAssured.standAlone();
-//   // }
+  @MockBean
+  private LaboratorioRepository labRepository;
 
-//   @Test
-//   public void deveConterUmaListaDeLaboratorios_QuandoChamarFindAll() {
-//     LaboratorioRepository repository;
-//     // when controller.findAll()
-//     // mock repository.findAll() devolve [];
-//     // return []
+  @Test
+  public void deveConterUmaListaDeLaboratorios_QuandoChamarFindAll() {
+    LaboratorioController controller = new LaboratorioController(labRepository);
+    
+    Mockito.when(labRepository.findAll()).thenReturn(new ArrayList<Laboratorio>());
+    
+    assertEquals(controller.getAll().size(), 0);
+    assertEquals(controller.getAll().getClass(), ArrayList.class);
+  }
 
+  @Test
+  public void deveConterUmaListaDeLaboratoriosComPeloMenosUmQuandoHouver_QuandoChamarFindAll() {
+    LaboratorioController controller = new LaboratorioController(labRepository);
+    Laboratorio lab = new Laboratorio(null, "Agrotis Teste", null);
+    
+    Mockito.when(labRepository.findAll()).thenReturn(new ArrayList<Laboratorio>(List.of(lab)));
+    
+    assertEquals(controller.getAll().size(), 1);
+    assertEquals(controller.getAll().getClass(), ArrayList.class);
+  }
 
-  // }
+  @Test
+  public void deveRetornarUmConflictQuandoReceberUmLabQueJaEstejaCadastrado_QuandoChamarFindAll()
+      throws Exception {
+    LaboratorioController controller = new LaboratorioController(labRepository);
+    Laboratorio lab = new Laboratorio(null, "Agrotis Teste", null);
+    
+    Mockito.when(labRepository.findByName("Agrotis Teste"))
+      .thenReturn(new ArrayList<Laboratorio>(List.of(lab)));
+    
+    final ResponseEntity<Laboratorio> response = controller.create(lab);
 
+    assertEquals(response.getStatusCode(), HttpStatus.CONFLICT);
+  }
 
 }
