1a01dec2252511bf4400606b4f98af9924f37267
Murilo Ribeiro <murilorsv14@gmail.com>
2022-06-05 20:51:50 -0300

    test: boiderplate para teste em controller e service de user

---
+++ src/test/java/com/agrotis/controllers/UserControllerTest.java
@@ -0,0 +1,13 @@
+package com.agrotis.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Teste de unidade para o controller de User")
+public class UserControllerTest {
+  
+}
+
---
+++ src/test/java/com/agrotis/services/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.agrotis.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Teste de unidade para o service de User")
+public class UserServiceTest {
+  
+}
